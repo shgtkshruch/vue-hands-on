7ac21e346236af1ad3717915306093d8942e3f1e
shgtkshruch <shgtk.shruch@gmail.com>
2018-04-20 20:18:35 +0900

    data bidning

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello World!'
+    firstName: '苗字',
+    lastName: '名前',
+    message: 'Hello World!',
   },
   mounted() {
-    this.message = 'mounted!';
+    setTimeout(() => {
+      this.message = 'mounted!';
+    }, 2000);
   },
   watch: {
     message(val, oldVal) {
       console.log('new: %s, old: %s', val, oldVal);
     }
   },
+  methods: {
+    input(event) {
+      this.message = event.target.value;
+    }
+  }
 });
