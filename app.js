ac3575a8359ae8758bdbae31e26180f936848fda
shgtkshruch <shgtk.shruch@gmail.com>
2018-04-20 19:28:56 +0900

    mount and watch

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello World!'
+  },
+  mounted() {
+    this.message = 'mounted!'
+  },
+  watch: {
+    message(val, oldVal) {
+      console.log('new: %s, old: %s', val, oldVal);
+    }
   }
 });
